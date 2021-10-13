fcf3c474b1316c9ae600824c45b1323ca77005b3
Lilia Tatarintseva <juicy-1_3@inbox.ru>
2021-10-13 13:08:45 +0400

    initial commit

---
+++ src/Main.java
@@ -0,0 +1,21 @@
+public class Main {
+    public static void main(String[] args) {
+        int currentbalance = 250;
+        int add = 1155;
+        int balance = currentbalance + add;
+        int bonus = 100;
+        int totalbonus = add / bonus;
+        if (add > 100) {
+            System.out.println(totalbonus);
+        }
+        System.out.println(balance);
+
+
+
+
+
+
+
+    }
+
+}
